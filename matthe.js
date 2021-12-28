cc232a078dfc8d935cf3ea5af2d1889336b1c53e
BoranGkdn <90551725+BoranGkdn@users.noreply.github.com>
2021-12-28 16:46:00 +0300

    Update matthe.js

--- matthe.js
+++ matthe.js
@@ -4,7 +4,7 @@ const config = require("./config.json");
 const önerilimit = new Map()
 
 client.on("ready", () => {
-    client.user.setPresence({ activity: { name: "Developed by Matthe" }, status: "online" });
+    client.user.setPresence({ activity: { name: "Developed by BoranGkdn" }, status: "online" });
 })
 client.on("ready", () => {
     const ses = client.channels.cache.get(config.seskanal);
@@ -22,7 +22,7 @@ client.on("message", async message => {
     if (message.guild) return;
     if (config.önerilimit > 0 && önerilimit.has(message.author.id) && önerilimit.get(message.author.id) == config.önerilimit) return message.channel.send("Saatlik öneri limitine ulaştın!");
     message.channel.send("Önerin başarıyla sunucu yetkililerine iletildi! Bir dahaki önerini 1 saat sonra yapabileceksin!")
-    client.channels.cache.get(config.logkanal).send(new Discord.MessageEmbed().setAuthor("Yeni Bir Öneri Var", client.user.avatarURL()).setFooter("Developed by Matthe").setDescription(`**Gönderenin Bilgileri:** ${message.author} - ${message.author.id}`).setTimestamp().addField("**Mesaj**", message.content).setColor("RANDOM"))
+    client.channels.cache.get(config.logkanal).send(new Discord.MessageEmbed().setAuthor("Yeni Bir Öneri Var", client.user.avatarURL()).setFooter("Developed by BoranGkdn").setDescription(`**Gönderenin Bilgileri:** ${message.author} - ${message.author.id}`).setTimestamp().addField("**Mesaj**", message.content).setColor("RANDOM"))
     if (1 > 0) {
         if (!önerilimit.has(message.author.id)) önerilimit.set(message.author.id, 1);
         else önerilimit.set(message.author.id, önerilimit.get(message.author.id) + 1);
@@ -32,4 +32,4 @@ client.on("message", async message => {
     };
 })
 
-client.login(config.token).then(() => console.log(`[ MATTHE ] ${client.user.username} olarak giriş yaptı!`)).catch(() => console.log(`[ MATTHE ] Bot giriş yapamadı!`));
+client.login(config.token).then(() => console.log(`[ MATTHE ] ${client.user.username} olarak giriş yaptı!`)).catch(() => console.log(`[ BORANGKDN ] Bot giriş yapamadı!`));
